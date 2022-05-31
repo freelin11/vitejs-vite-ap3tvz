d7fad426dfb76755dbd4b7648573c371e61e1d32
freelin11 <freelin11@gmail.com>
2022-05-31 10:14:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-ap3tvz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-ap3tvz)
---
+++ main.js
@@ -0,0 +1,6 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <h1>Helló Világ!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Dokumentáció</a>
+`
